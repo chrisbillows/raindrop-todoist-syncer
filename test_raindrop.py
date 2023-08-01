da817ac908d37fccbab5c17810095bdb2020e4ef
chrisbillows <christopherbillows@gmail.com>
2023-08-01 14:19:11 +0100

    Add test_raindrop.py
    Add tests for WIP refactor of GetNewOauth

---
+++ test_raindrop.py
@@ -0,0 +1,110 @@
+from unittest.mock import patch
+import pytest
+
+from raindrop import RaindropGetNewOauth
+
+
+@pytest.fixture
+def raindrop_oauth():
+    """
+    Used by the following series of test classes which test the RoamApiInterface
+    """
+    return RaindropGetNewOauth()
+
+
+class TestOauthInit:
+           
+    def test_rd_client_id_not_none(self, raindrop_oauth):
+        assert raindrop_oauth.RAINDROP_CLIENT_ID != None
+        
+    def test_client_id_familiar_length(self, raindrop_oauth):
+        """
+        Client id may not always be 24 char
+        """
+        assert len(raindrop_oauth.RAINDROP_CLIENT_ID) == 24
+
+    def test_client_id_alpha_numeric(self, raindrop_oauth):
+        """
+        Client ID may not always be alnum
+        """
+        assert raindrop_oauth.RAINDROP_CLIENT_ID.isalnum() == True
+    
+    def test_rd_client_secret_not_none(self, raindrop_oauth):
+        assert raindrop_oauth.RAINDROP_CLIENT_SECRET != None
+
+    def test_rd_client_secret_familiar_length(self, raindrop_oauth):
+        assert len(raindrop_oauth.RAINDROP_CLIENT_SECRET) == 36
+
+    def test_client_secret_uuid_format(self, raindrop_oauth):
+        import re
+
+        def is_uuid(value):
+            uuid_regex = re.compile(
+                r'^[0-9a-f]{8}-'
+                r'[0-9a-f]{4}-'
+                r'4[0-9a-f]{3}-'
+                r'[89ab][0-9a-f]{3}-'
+                r'[0-9a-f]{12}\Z', re.I)
+            return bool(uuid_regex.match(value))
+
+        assert is_uuid(raindrop_oauth.RAINDROP_CLIENT_SECRET) == True
+
+    def test_redirect_uri_correct(self, raindrop_oauth):
+        assert raindrop_oauth.REDIRECT_URI == "http://localhost"
+    
+    def test_base_url_correct(self, raindrop_oauth):
+        assert raindrop_oauth.AUTH_CODE_BASE_URL == "https://raindrop.io/oauth/authorize"
+        
+    def test_headers_correct(self, raindrop_oauth):
+        assert raindrop_oauth.HEADERS == {"Content-Type": "application/json"}
+
+        
+class TestOpenAuthCodeUrl:
+        
+    @patch('webbrowser.open')
+    def test_open_authorization_code_url_creates_url_correctly(self, mock_webbrowser_open, raindrop_oauth):
+        raindrop_oauth._open_authorization_code_url()
+        expected_url = f"https://raindrop.io/oauth/authorize?client_id={raindrop_oauth.RAINDROP_CLIENT_ID}&redirect_uri={raindrop_oauth.REDIRECT_URI}"
+        actual_url = mock_webbrowser_open.call_args[0][0]
+        assert actual_url == expected_url    
+
+
+class TestUserPasteValidAuthCodeUrl:
+    
+    def test_user_paste_valid_input(self, raindrop_oauth):
+        with patch('builtins.input', return_value='http://localhost/?code=aa4c0bc8-0e19-4615-a032-bd3379829ca7'):
+            result = raindrop_oauth._user_paste_valid_auth_code_url()
+        assert result == 'http://localhost/?code=aa4c0bc8-0e19-4615-a032-bd3379829ca7'
+
+    def test_user_paste_valid_auth_code_url_invalid_then_valid_input(self, raindrop_oauth):
+        with patch('builtins.input', side_effect=['invalid_url', 'http://localhost/?code=aa4c0bc8-0e19-4615-a032-bd3379829ca7']):
+            raindrop = RaindropGetNewOauth()
+            result = raindrop_oauth._user_paste_valid_auth_code_url()
+            assert result == 'http://localhost/?code=aa4c0bc8-0e19-4615-a032-bd3379829ca7'
+ 
+class TestParseAuthorizationCodeUrl:
+     
+    def test_parse_auth_code_url_valid(self, raindrop_oauth):
+        authorization_code = raindrop_oauth._parse_authorization_code_url('http://localhost/?code=aa4c0bc8-0e19-4615-a032-bd3379829ca7')
+        expected_authorization_code = 'aa4c0bc8-0e19-4615-a032-bd3379829ca7'
+        assert authorization_code == expected_authorization_code
+        
+    def test_parse_auth_code_url_alternate_code_format_length(self, raindrop_oauth):
+        authorization_code = raindrop_oauth._parse_authorization_code_url('http://localhost/?code=aa4c0bc8')
+        expected_authorization_code = 'aa4c0bc8'
+        assert authorization_code == expected_authorization_code
+        
+class TestCreateBody:
+    
+    def test_create_body_valid(self, raindrop_oauth):
+        authorization_code = "1234"
+        expected_body = {
+                "grant_type": "authorization_code",
+                "code": authorization_code,
+                "client_id": self.RAINDROP_CLIENT_ID,
+                "client_secret": self.RAINDROP_CLIENT_SECRET,
+                "redirect_uri": "http://localhost",
+        }
+        body = raindrop_oauth._create_body(authorization_code)
+        assert body == expected_body
+        
