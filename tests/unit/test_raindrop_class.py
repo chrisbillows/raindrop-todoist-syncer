2ad778cf39ee9d3af4237a7b0e89efcb7cd15e78
chrisbillows <christopherbillows@gmail.com>
2023-08-09 17:46:18 +0100

    First working test on test_raindrop_class. And add init pys.

--- tests/unit/test_raindrop_class.py
+++ tests/unit/test_raindrop_class.py
@@ -1,72 +1,58 @@
 from unittest import mock
+import json
 import pytest
 
 from raindrop import Raindrop
 
 
-@pytest.fixture
-def raindrop_class():
-   return Raindrop()
-
-@pytest.fixture
-def raindrop_body():
-    rd = mock.Mock()
-    rd.id = ""
-    rd.created_time = ""
-    rd.parsed_time = ""
-    rd.title = ""
-    rd.notes = ""
-    rd.link = ""
-    return rd
+# @pytest.fixture
+# def raindrop_body():
+#     rd = mock.Mock()
+#     rd.id = ""
+#     rd.created_time = ""
+#     rd.parsed_time = ""
+#     rd.title = ""
+#     rd.notes = ""
+#     rd.link = ""
+#     return rd
     
 @pytest.fixture
 def rd_raw_api_response():
     requests_obj = mock.Mock()
-    requests_obj.status_code = "200"
-    # ...
+    requests_obj.status_code = 200
+    requests_obj.text = "This is some status code text."
+    with open('tests/unit/rd_api_full_dot_json_.json', "r") as f:
+        content = json.load(f)
+        requests_obj.json = mock.Mock(return_value=content)
     return requests_obj
-    
-# RAW RAINDROP EXAMPLE
-"""
-{'_id': 621872658,
- 'broken': False,
- 'cache': {'created': '2023-08-06T19:57:10.000Z',
-           'size': 182846,
-           'status': 'ready'},
- 'collection': {'$id': 31352328, '$ref': 'collections', 'oid': 31352328},
- 'collectionId': 31352328,
- 'cover': 'https://opengraph.githubassets.com/e1bbb2f1946ae6b1b3182a2a7e226114ffff058910f8426d1aad736dff6b513d/tadashi-aikawa/shukuchi',
- 'created': '2023-08-06T19:56:44.948Z',
- 'creatorRef': {'_id': 645158,
-                'avatar': '',
-                'email': '',
-                'name': 'christopherbillows'},
- 'domain': 'github.com',
- 'excerpt': 'Shukuchi is an Obsidian plugin that enables you to teleport to '
-            'links (URL or internal link). - tadashi-aikawa/shukuchi: Shukuchi '
-            'is an Obsidian plugin that enables you to teleport to links (URL '
-            'or ...',
- 'highlights': [],
- 'important': False,
- 'lastUpdate': '2023-08-06T19:56:46.806Z',
- 'link': 'https://github.com/tadashi-aikawa/shukuchi',
- 'media': [{'link': 'https://opengraph.githubassets.com/e1bbb2f1946ae6b1b3182a2a7e226114ffff058910f8426d1aad736dff6b513d/tadashi-aikawa/shukuchi',
-            'type': 'image'},
-           {'link': 'https://raw.githubusercontent.com/tadashi-aikawa/shukuchi/master/resources/direction-of-possible-teleportation.png',
-            'type': 'image'}],
- 'note': '',
- 'reminder': {'date': None},
- 'removed': False,
- 'sort': 621872658,
- 'tags': [],
- 'title': 'tadashi-aikawa/shukuchi: Shukuchi is an Obsidian plugin that '
-          'enables you to teleport to links (URL or internal link).',
- 'type': 'link',
- 'user': {'$id': 645158, '$ref': 'users'}}
-""" 
+   
+
+@pytest.fixture
+def rd_extracted_single_raindrop_dict():
+    """
+    Returns a dictionary representation of a single raindrop. 
+    In actual usage, this structure is typically extracted from a list of 
+    raindrops that have been processed with json.load. For testing purposes, 
+    this fixture loads the content of a single raindrop saved as JSON from a file.
+    """
+    with open('tests/unit/rd_api_single_rd.json', "r") as f:
+        content = json.load(f)
+    return content
+
+@pytest.fixture
+def raindrop_object(rd_extracted_single_raindrop_dict):
+   return Raindrop(rd_extracted_single_raindrop_dict)
+
+class TestInit:
+    """
+    Early warning system for my own idiocy.    
+    """    
+    def test_init_id(self, raindrop_object):
+        assert raindrop_object.id == 621872658
+   
 
 """
-OUR JSON EXAMPLE
+RD OBJECT EXAMPLE
     {
         "id": 588482518,
         "created_time": "2023-06-09T14:45:58.831Z",
@@ -77,13 +63,4 @@ OUR JSON EXAMPLE
     }
 """
 
-
-class TestInit:
-    """
-    Early warning system for my own idiocy.    
-    """    
-    
-    def test_init_id():
-        pass
-    
     
