bc790e6704dc487cd6f6d9ce83ed147582afe101
chrisbillows <christopherbillows@gmail.com>
2023-08-18 13:30:36 +0100

    Add TestCoreApiCall and TestMakeApiCall. Refactor and remove some defunct tests post api refactor.

--- tests/unit/test_rd_client.py
+++ tests/unit/test_rd_client.py
@@ -1,7 +1,10 @@
 import random
-from unittest import mock
+from unittest.mock import patch, Mock
 from urllib import response
+
 import pytest
+from requests import HTTPError
+import tenacity
 
 # from raindrop import RaindropClient
 from raindrop import RaindropClient_dev
@@ -57,44 +60,156 @@ class TestInit:
 #         assert ids == expected_ids
 
 
+class TestCoreApiCall:
+    """
+    Tests for the core API call, without tenacity's retries, waits etc.
+    """
+    @pytest.mark.parametrize("page", [0, 1])
+    def test__core_api_call_cached_responses_not_none(
+        self, mock_requests_get, rd_client, page
+    ):
+        response = rd_client._core_api_call(page)
+        assert response is not None
+
+    @pytest.mark.parametrize("page", [0, 1])
+    def test__core_api_call_cached_responses_status_200(
+        self, mock_requests_get, rd_client, page
+    ):
+        response_status = rd_client._core_api_call(page).status_code
+        assert response_status == 200
+
+    def test_core_api_call_cached_responses_missing_pg_exception(
+        self, mock_requests_get, rd_client
+    ):
+        with pytest.raises(HTTPError):
+            rd_client._core_api_call(10).status_code
+          
+    
+    @pytest.mark.parametrize(
+    "page, key_path, expected_value",
+    [
+        (0, ["result"], True),
+        (1, ["result"], True),
+        (0, ["items", 0, "creatorRef", "name"], "christopherbillows"),
+        (1, ["items", 0, "creatorRef", "name"], "christopherbillows"),
+        (0, ["count"], 26),
+        (1, ["count"], 26),
+        (0, ["collectionId"], 36697540),
+        (1, ["collectionId"], 36697540),
+    ],
+    )
+    def test_core_call_cached_responses(self, 
+    mock_requests_get, rd_client, page, key_path, expected_value
+    ):
+        response = rd_client._core_api_call(page)
+        json_data = response.json()
+        for key in key_path:
+            json_data = json_data[key]
+        assert json_data == expected_value
+
+
+class TestMakeApiCall:
+    """
+    Tests for the full API call, including the tenacity retry logic.
+    """
+
+    @pytest.mark.parametrize("page", [0, 1])
+    def test__make_api_call_cached_responses_not_none(
+        self, mock_requests_get, rd_client, page
+    ):
+        response = rd_client._make_api_call(page)
+        assert response is not None
+
+    @pytest.mark.parametrize("page", [0, 1])
+    def test__make_api_call_cached_responses_status_200(
+        self, mock_requests_get, rd_client, page
+    ):
+        response_status = rd_client._make_api_call(page).status_code
+        assert response_status == 200
+
+    @pytest.mark.parametrize(
+    "page, key_path, expected_value",
+    [
+        (0, ["result"], True),
+        (1, ["result"], True),
+        (0, ["items", 0, "creatorRef", "name"], "christopherbillows"),
+        (1, ["items", 0, "creatorRef", "name"], "christopherbillows"),
+        (0, ["count"], 26),
+        (1, ["count"], 26),
+        (0, ["collectionId"], 36697540),
+        (1, ["collectionId"], 36697540),
+    ],
+    )
+    def test_make_api_call_cached_responses(self, 
+    mock_requests_get, rd_client, page, key_path, expected_value
+    ):
+        response = rd_client._make_api_call(page)
+        json_data = response.json()
+        for key in key_path:
+            json_data = json_data[key]
+        assert json_data == expected_value    
+        
+    @pytest.mark.parametrize(
+        "status_code, exception, match_str, call_count",
+        [
+            (200, None, None, 1),                 # 1 call, no retries
+            (403, tenacity.RetryError, None, 3),  # 3 retries before raising RetryError
+            (404, tenacity.RetryError, None, 3),
+            (500, tenacity.RetryError, None, 3),
+        ],
+    )
+    def test_makes_api_call_retries_for_various_status_codes(
+        self, mocker, rd_client, status_code, exception, match_str, call_count
+    ):
+        """ 
+        Tests handling of different status codes including retry logic.
+        """
+        mock_response = Mock()
+        mock_response.status_code = status_code
+        if status_code != 200:
+            mock_response.raise_for_status.side_effect = HTTPError(
+                f"API Error: {status_code}"
+            )
+        mock_get = mocker.patch("requests.get")
+        mock_get.return_value = mock_response
+        if exception:
+            with pytest.raises(exception, match=match_str):
+                rd_client._make_api_call(0)
+            assert mock_get.call_count == call_count
+        else:
+            assert rd_client._make_api_call(0) == mock_response
+            assert mock_get.call_count == call_count
+    
+    
+
+
 class TestDev:
     """
     Additional test for refactored RaindropClass
 
     To be combined with other tests once everything works correctly.
     """
+   
+    # @pytest.mark.parametrize(
+    #     "status_code, exception, match_str",
+    #     [
+    #         (200, None, None),
+    #         (403, ValueError, "API Error: 403"),
+    #         (404, ValueError, "API Error: 404"),
+    #         (500, ValueError, "API Error: 500"),
+    #     ],
+    # )
+    # def test__response_validator(self, rd_client, status_code, exception, match_str):
+    #     """
 
-    @pytest.mark.parametrize("page", [0, 1, 2])
-    def test__make_api_call_not_none(self, mock_requests_get, rd_client, page):
-        response = rd_client._make_api_call(page)
-        assert response is not None
-
-    @pytest.mark.parametrize("page", [0, 1])
-    def test__make_api_call_status_200(self, mock_requests_get, rd_client, page):
-        response_status = rd_client._make_api_call(page).status_code
-        assert response_status == 200
-
-    def test__make_api_call_status_404(self, mock_requests_get, rd_client):
-        response_status = rd_client._make_api_call(2).status_code
-        assert response_status == 404
-
-    @pytest.mark.parametrize(
-        "status_code, exception, match_str",
-        [
-            (200, None, None),
-            (403, ValueError, "API Error: 403"),
-            (404, ValueError, "API Error: 404"),
-            (500, ValueError, "API Error: 500"),
-        ],
-    )
-    def test__response_validator(self, rd_client, status_code, exception, match_str):
-        mock_response = mock.Mock()
-        mock_response.status_code = status_code
-        if exception:
-            with pytest.raises(exception, match=match_str):
-                rd_client._response_validator(mock_response)
-        else:
-            assert rd_client._response_validator(mock_response) is None
+    #     """
+    #     mock_response = Mock()
+    #     mock_response.status_code = status_code
+    #     if exception:
+    #         with pytest.raises(exception, match=match_str):
+    #             rd_client._response_validator(mock_response)
+    #     else:
+    #         assert rd_client._response_validator(mock_response) is None
 
     @pytest.mark.parametrize("fixture_name", ["response_one_data", "response_two_data"])
     def test_extract_benchmark_count(self, request, rd_client, fixture_name):
@@ -189,7 +304,7 @@ class TestDev:
         rds = rds
         with pytest.raises(ValueError, match=match_str):
             rd_client._individual_rd_validator(rds)
-``
+
     # def test__individual_rd_validator_manual_ids_fail_empty(self, rd_client):
     #     rds = [{"_id": ""}, {"_id": 456}, {"_id": 789}]
     #     #! Don't we want this to fail?
@@ -199,27 +314,58 @@ class TestDev:
     #     ):
     #         rd_client._individual_rd_validator(rds)
 
-    def test__individual_rd_validator_manual_ids_fail_none(self, rd_client):
-        rds = [{"_id": None}, {"_id": 456}, {"_id": 789}]
+    @pytest.mark.parametrize(
+        "benchmark_count, exception, match_str",
+        [
+            (26, None, None),
+            (27, ValueError, "Total raindrops extracted not expected length."),
+        ],
+    )
+    def test_cumulative_rds_validator(
+        self,
+        rd_client,
+        exception,
+        match_str,
+        response_one_data,
+        response_two_data,
+        benchmark_count,
+    ):
+        current_rds = response_two_data["items"]
+        cumulative_rds = response_one_data["items"] + current_rds
+        bmc = benchmark_count
+        if exception is not None:
+            with pytest.raises(exception, match=match_str):
+                rd_client._cumulative_rds_validator(cumulative_rds, current_rds, bmc)
+        else:
+            assert (
+                rd_client._cumulative_rds_validator(cumulative_rds, current_rds, bmc)
+                is None
+            )
+
+    def test_cumulative_rds_validator_manual_pass(self, rd_client):
+        cumulative_rds = list(range(1772))
+        current_rds = list(range(22))
+        bm = 1772
+        assert (
+            rd_client._cumulative_rds_validator(cumulative_rds, current_rds, bm) is None
+        )
+
+    def test_cumulative_rds_validator_manual_fail(self, rd_client):
+        cumulative_rds = list(range(1772))
+        current_rds = list(range(22))
+        bm = 1771
         with pytest.raises(
-            ValueError, match="Invalid raindrop item found in current collection."
+            ValueError, match="Total raindrops extracted not expected length."
         ):
-            rd_client._individual_rd_validator(rds)
+            rd_client._cumulative_rds_validator(cumulative_rds, current_rds, bm)
 
-    def test__individual_rd_validator_manual_ids_fail_missing(self, rd_client):
-        rds = [{"_id": 123}, {"_id": 456}, {"other_key": "other_value"}]
+    def test_cumulative_rds_last_pg_wrong_length(self, rd_client):
+        cumulative_rds = list(range(53))
+        current_rds = list(range(25))
+        bm = 53
+        expected_len_last_page = bm % rd_client.RAINDROPS_PER_PAGE
         with pytest.raises(
-            ValueError, match="Invalid raindrop item found in current collection."
+            ValueError,
+            match=f"Last page results not expected length. Expected: {expected_len_last_page}, Got: {len(current_rds)}",
         ):
-            rd_client._individual_rd_validator(rds)
-
-    def test_cumulative_rds_validator(self):
-        pass
-
-
-# def test__make_api_call_response_has_no_status(self, mock_requests_get_no_status, rd_client):
-#! Lets just ditch this - I don't even know what I'm trying to do here!
-#     response = rd_client._make_api_call(0)
-#     with pytest.raises(AttributeError):
-#         # _ = response.status_code
-#         _ = response.non_existent_attribute
+            rd_client._cumulative_rds_validator(cumulative_rds, current_rds, bm)
