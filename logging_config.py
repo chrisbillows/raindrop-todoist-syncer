24d4817c5e89c964b2089adacea55822a7a36973
chrisbillows <christopherbillows@gmail.com>
2023-08-24 12:14:45 +0100

    Configure loguru to intercept third party logs
    Intercept all logs sent to the built in logger. Write to seperate file for ongoing monitoring if required.

--- logging_config.py
+++ logging_config.py
@@ -1,9 +1,34 @@
-from loguru import logger
+import logging
 import sys
+from loguru import logger
+
+class InterceptHandler(logging.Handler):
+    """
+    Class to re-route logs created by third party modules to loguru.
+    """
+    def emit(self, record):
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        logger.opt(depth=6, exception=record.exc_info).log(level, record.getMessage())
+
 
 def configure_logging():
     logger.remove(0)  # Remove loguru's default logger
-
+    
+    # Intercept third party logs 
+    logging.basicConfig(handlers=[InterceptHandler()], level=0)
+    logging.root.setLevel(logging.DEBUG)
+    
+    # Configure seperate file handling if req'd e.g. for monitoring third party modules
+    # for debugging notifications I'm unaware of
+    file_handler = logging.FileHandler('builtin_logging.log')
+    file_handler.setLevel(logging.DEBUG)
+    logging.root.addHandler(file_handler)
+    
+    
     logger.add(
         sys.stdout,
         format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}",
