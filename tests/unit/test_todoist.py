b22e42227afdd9b8ec249a5294fa4dea02671ce7
chrisbillows <christopherbillows@gmail.com>
2023-11-16 20:50:41 +0000

    Add TodoistTraskCreatorDev WIP function + tests

---
+++ tests/unit/test_todoist.py
@@ -0,0 +1,54 @@
+import json
+from unittest import mock
+from unittest.mock import patch
+
+import pytest
+
+from raindrop import RaindropsProcessor
+from todoist import TodoistTaskCreatorDev
+
+@pytest.fixture
+def todoist_task_creator():
+    return TodoistTaskCreatorDev()
+
+@pytest.fixture
+def list_of_rd_objects():
+    """
+    Create a list of three Raindrop objects.
+    """
+    with open("tests/unit/cumulative_rd_list.json", "r") as f:
+        rds = json.load(f)
+    rd_sample = rds[2:5]
+    rdp = RaindropsProcessor(rd_sample)
+    rd_objects = rdp._convert_to_rd_objects(rdp.all_rds)
+    return rd_objects
+
+
+class TestInit:
+    
+    def test_main_work_project(self, todoist_task_creator):
+        assert todoist_task_creator.MAIN_WORK_PROJECT == "2314091414"
+
+    def test_main_work_api_key(self, todoist_task_creator):
+        assert todoist_task_creator.TODOIST_API_KEY is not None
+
+class TestCreateTask:
+    
+    def test_create_task_actual_rd_objects(self, todoist_task_creator, list_of_rd_objects):
+        rd_object = list_of_rd_objects[2]
+       
+        with patch('todoist.TodoistAPI.add_task') as mock_add_task:
+            todoist_task_creator.create_task(rd_object)
+            mock_add_task.assert_called_once_with(
+                content='**Obsidian - Sharpen your thinking**',
+                project_id=TodoistTaskCreatorDev.MAIN_WORK_PROJECT,
+                description='',
+                due_string='today',
+                due_lang='en',
+                priority=1,
+                labels=['Raindrop']
+                )
+            
+        
+
+
--- todoist.py
+++ todoist.py
@@ -1,4 +1,5 @@
 import os
+from typing import List
 
 from dotenv import load_dotenv
 from loguru import logger
@@ -98,3 +99,81 @@ class TodoistTaskCreator:
             comment = self.api.add_comment(task_id=task_id, content=self.website_link)
         except Exception as e:
             logger.error(e)
+
+class TodoistTaskCreatorDev:
+    """
+    A class to create Todoist tasks from a Raindrop object.
+
+    Attributes
+    ----------
+    MAIN_WORK_PROJECT : str
+        a formatted string to hold the main work project ID
+    .TODOIST_API_KEY : str
+        a string representing the Todoist API key
+    api : TodoistAPI
+        an instance of the TodoistAPI class
+    task_title : str
+        a string representing the task title
+    task_description : str
+        a string representing the task description
+    website_link : str
+        a string representing the website link
+
+    Methods
+    -------
+    create_task():
+        Creates a new task in Todoist.
+    _add_link_as_comment(task_id: str):
+        Adds the Raindrops website link as a comment to the task.
+    """
+    MAIN_WORK_PROJECT = "2314091414"
+    TODOIST_API_KEY = os.getenv("TODOIST_API_KEY")
+
+    def __init__(self) -> None:
+        """
+        """
+        self.api = TodoistAPI(self.TODOIST_API_KEY)
+
+    def create_tasks(self, raindrops: List[Raindrop]) -> None:
+        for rd in raindrops:
+            self.create_task(rd)
+            self._add_link_as_comment(rd["_id"])
+            logger.info("Task created %s", rd['title'])            
+
+    def create_task(self, single_rd_obj: Raindrop):
+        """
+        Creates a new task in Todoist.
+        """
+        task_content = single_rd_obj.title
+        task_description = single_rd_obj.notes
+        try:
+            task = self.api.add_task(
+                content=f"**{task_content}**",
+                project_id=self.MAIN_WORK_PROJECT,
+                description=f"{task_description}",
+                due_string="today",
+                due_lang="en",
+                priority=1,
+                labels=["Raindrop"],
+            )
+        # TODO: This needs to be improved.
+        except Exception as e:
+            logger.error(e)
+        
+
+    def _add_link_as_comment(self, task_id):
+        """
+        Adds the Raindrop's website link as a comment to the task. This is added as a
+        comment and not to the description because Todoist renders a preview for links
+        in the comments (not for all links).
+
+        Parameters
+        ----------
+        task_id : str
+            The id of the task
+        """
+        try:
+            comment = self.api.add_comment(task_id=task_id, content=self.website_link)
+        except Exception as e:
+            logger.error(e)
+
