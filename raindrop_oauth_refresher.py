70dce806399ff429e5510201a403997df2613cb7
chrisbillows <christopherbillows@gmail.com>
2023-12-01 17:34:36 +0000

    Add comment that old token isn't overwritten

--- raindrop_oauth_refresher.py
+++ raindrop_oauth_refresher.py
@@ -3,6 +3,8 @@ from raindrop import RaindropOauthHandler, MissingRefreshTokenError
 """
 Temp file just as a place to run oauth referesher before automating. DELETE WHEN THAT 
 IS BUILT.
+
+THIS DOESN'T REMOVE OLD TOKEN!
 """
 
 if __name__ == "__main__":
