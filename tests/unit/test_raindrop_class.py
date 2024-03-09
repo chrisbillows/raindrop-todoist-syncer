ff5f1e27b17b8af794c108c9b170603df55cfc1c
chrisbillows <christopherbillows@gmail.com>
2024-03-09 21:10:58 +0000

    #37: Fix tests.

--- tests/unit/test_raindrop_class.py
+++ tests/unit/test_raindrop_class.py
@@ -1,68 +1,37 @@
-from unittest import mock
-import json
-import pytest
-
-
-# @pytest.fixture
-# def raindrop_body():
-#     rd = mock.Mock()
-#     rd.id = ""
-#     rd.created_time = ""
-#     rd.parsed_time = ""
-#     rd.title = ""
-#     rd.notes = ""
-#     rd.link = ""
-#     return rd
-
-
-@pytest.fixture
-def rd_raw_api_response():
-    requests_obj = mock.Mock()
-    requests_obj.status_code = 200
-    requests_obj.text = "This is some status code text."
-    with open("tests/mock_data/rd_api_full_dot_json_.json", "r") as f:
-        content = json.load(f)
-        requests_obj.json = mock.Mock(return_value=content)
-    return requests_obj
-
-
 class TestInit:
     """
     Early warning system for my own idiocy.
     """
 
     def test_init_id(self, raindrop_object):
-        assert raindrop_object.id == 621872658
+        assert raindrop_object.id == 628161672
 
     def test_init_created_time(self, raindrop_object):
-        assert raindrop_object.created_time == "2023-08-06T19:56:44.948Z"
+        assert raindrop_object.created_time == "2023-08-14T09:36:24.856Z"
 
     def test_parsed_date_not_none(self, raindrop_object):
         assert raindrop_object.parsed_time is not None
 
     def test_init_title(self, raindrop_object):
-        assert (
-            raindrop_object.title
-            == "tadashi-aikawa/shukuchi: Shukuchi is an Obsidian plugin that enables you to teleport to links (URL or internal link)."
-        )
+        assert raindrop_object.title == "Welcome to Python.org"
 
     def test_init_notes(self, raindrop_object):
         assert raindrop_object.notes == ""
 
     def test_init_link(self, raindrop_object):
-        assert raindrop_object.link == "https://github.com/tadashi-aikawa/shukuchi"
+        assert raindrop_object.link == "https://www.python.org/"
 
 
 class TestToDict:
     def test_to_dict(self, raindrop_object):
         raindrop_object.parsed_time = "2023-08-01T01:01:01.001Z"
         expected_result = {
-            "id": 621872658,
-            "created_time": "2023-08-06T19:56:44.948Z",
+            "id": 628161672,
+            "created_time": "2023-08-14T09:36:24.856Z",
             "parsed_time": "2023-08-01T01:01:01.001Z",
-            "title": "tadashi-aikawa/shukuchi: Shukuchi is an Obsidian plugin that enables you to teleport to links (URL or internal link).",
+            "title": "Welcome to Python.org",
             "notes": "",
-            "link": "https://github.com/tadashi-aikawa/shukuchi",
+            "link": "https://www.python.org/",
         }
         assert raindrop_object.to_dict() == expected_result
 
