8a25714017eaf1231d3f0d57afe48c5543f353d1
chrisbillows <christopherbillows@gmail.com>
2024-03-31 15:21:28 +0100

    #50: Delete unused `run` function.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import datetime
-import time
 import traceback
 from loguru import logger
 
@@ -43,60 +42,7 @@ def main():
         dbm.update_database([task])
 
 
-def run():
-    """
-    Function that runs the main function and handles exceptions. Asks the user for:
-    a) number of runs: [int]
-    b) wait between runs in seconds [int]
-
-    (Four hours = 480 runs)
-
-    If an error occurs, currently caught and logged (in case of temporary issues with
-    Raindrop or Todoist APIs, for example).
-    """
-    while True:
-        try:
-            runs = input("Enter the number of runs to complete: ")
-            runs = int(runs)
-            break
-        except ValueError:
-            logger.warning(f"{runs} is not a valid integer. Please try again.")
-
-    while True:
-        try:
-            wait_time = input("Enter the wait time between runs in seconds: ")
-            wait_time = int(wait_time)
-            break
-        except ValueError:
-            logger.warning(f"{wait_time} is not a valid integer. Please try again.")
-
-    logger.info(f"User selected {runs} runs and {wait_time} wait_time.")
-    completed_runs = 0
-    while completed_runs < runs:
-        completed_runs += 1
-        start = datetime.now()
-        logger.info(f"Run {completed_runs}/{runs} started.")
-        try:
-            main()
-            end_time = datetime.now() - start
-            logger.info(
-                f"Run {completed_runs}/{runs} completed in {end_time:.2f} seconds"
-            )
-            time.sleep(wait_time)
-
-        except Exception as e:
-            logger.error(f"{e}\n" f"{traceback.format_exc()}\n")
-            if completed_runs < runs:
-                logger.info(
-                    f"ERROR. CODE WILL RE-TRY IN {wait_time} AGAIN. {runs - completed_runs} RUN ATTEMPTS REMAIN."
-                )
-                time.sleep(wait_time)
-
-
 if __name__ == "__main__":
-    #! run commented out for plist
-    # TODO replace with argparse optionality
-    # run()
     start = datetime.datetime.now()
     logger.info(f"Run started at {start.strftime('%Y-%m-%d %H:%M:%S')}")
     try:
